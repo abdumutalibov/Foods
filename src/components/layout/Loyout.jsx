41e25e1bc16682c912159653ddbb122ff10125d0
Abduali <dreyfus1309@gmail.com>
2021-09-23 02:15:31 +0500

    TopNav

--- src/components/layout/Loyout.jsx
+++ src/components/layout/Loyout.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import './layout.css'
-import Sidebar from '../sidebar/Sidebar'
-import Routes from '../Routes'
+import React from "react";
+import "./layout.css";
+import Sidebar from "../sidebar/Sidebar";
+import TopNav from "../topnav/TopNav";
+import Routes from "../Routes";
 
-import {BrowserRouter,Route} from 'react-router-dom'
+import { BrowserRouter, Route } from "react-router-dom";
 const Loyout = () => {
-    return (
-        <BrowserRouter>
-           <Route render={(props)=>(
-               <div className='layout'>
-                   <Sidebar {...props}/>
-                   <div className='layout__content'>
-                       <div className='layout__content-main'>
-                           <Routes/>
-                       </div>
-                   </div>
-               </div>
-           )}/>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <div className="layout">
+            <Sidebar {...props} />
+            <div className="layout__content">
+              <TopNav />
+              <div className="layout__content-main">
+                <Routes />
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </BrowserRouter>
+  );
+};
 
-export default Loyout
+export default Loyout;
